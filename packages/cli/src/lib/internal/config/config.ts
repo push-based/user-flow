9ecddbfa13436018049c20d2fb51aa447e49d39e
Michael Hladky <michael@hladky.at>
2022-04-10 09:49:58 +0200

    fix: remove import

--- packages/cli/src/lib/internal/config/config.ts
+++ packages/cli/src/lib/internal/config/config.ts
@@ -1,6 +1,6 @@
 import { UserFlowRcConfig } from '../../types/model';
 import { readFile, writeFile } from '../utils/file';
-import { index } from '../utils/prettier/format-code';
+
 import { logVerbose } from '../../core/loggin';
 import { get as getRcPath } from '../../core/options/rc';
 
@@ -18,7 +18,7 @@ export function updateRepoConfig(config: UserFlowRcConfig, cfgPath: string = '')
   // Touch a file only if needed
 
   if (JSON.stringify(readRcConfig()) !== JSON.stringify(config)) {
-    writeFile(configPath, index(JSON.stringify(config), 'json'));
+    writeFile(configPath, JSON.stringify(config));
     logVerbose(`New config ${JSON.stringify(config)}`);
   } else {
     logVerbose(`No updates for ${configPath} to save.`);
