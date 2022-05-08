ea287ca91cf23078b42b4e16fa544b2f0ff05cab
Michael Hladky <michael@hladky.at>
2022-05-08 15:14:44 +0200

    docs: add example flows to project

--- packages/user-flow-example/README.md
+++ packages/user-flow-example/README.md
@@ -1,11 +1,4 @@
 # user-flow-example
 
-This library was generated with [Nx](https://nx.dev).
+This folder contains the user flow examples mentioned in the docs.
 
-## Building
-
-Run `nx build user-flow-example` to build the library.
-
-## Running unit tests
-
-Run `nx test user-flow-example` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/user-flow-example/user-flow/order-coffee-2.uf.ts
@@ -0,0 +1,50 @@
+import {
+  UserFlowInteractionsFn,
+  UserFlowContext,
+  UserFlowProvider
+} from '@push-based/user-flow';
+
+// Your custom interactions with the page
+const interactions: UserFlowInteractionsFn = async (ctx: UserFlowContext): Promise<any> => {
+  const { page, flow, browser, collectOptions } = ctx;
+  const { url } = collectOptions;
+
+  await flow.navigate(url, {
+    stepName: 'Navigate to coffee cart',
+  });
+
+
+  // Select coffee
+  const cappuccinoItem = '[data-test=Cappucino]';
+  await page.waitForSelector(cappuccinoItem);
+  await page.click(cappuccinoItem);
+
+  // Checkout order
+  const checkoutBtn = '[data-test=checkout]';
+  await page.waitForSelector(checkoutBtn);
+  await page.click(checkoutBtn);
+
+  const nameInputSelector = '#name';
+  await page.waitForSelector(nameInputSelector);
+  await page.type(nameInputSelector, 'nina');
+
+  const emailInputSelector = '#email';
+  await page.waitForSelector(emailInputSelector);
+  await page.type(emailInputSelector, 'nina@gmail.com');
+
+  // Submit order
+  const submitBtn = '#submit-payment';
+  await page.click(submitBtn);
+  await page.waitForSelector(submitBtn);
+  const successMsg = '.snackbar.success';
+  await page.waitForSelector(successMsg);
+
+
+};
+
+const userFlowProvider: UserFlowProvider = {
+  flowOptions: {name: 'Order Coffee'},
+  interactions
+};
+
+module.exports = userFlowProvider;
