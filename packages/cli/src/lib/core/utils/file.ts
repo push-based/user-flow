b22bf3bf243b4240087b8737c28d4338a379e64e
Julian Jandl <julian@jandl.wien>
2022-05-11 01:22:57 +0200

    test(cli): adjust ts-node configuration

--- packages/cli/src/lib/core/utils/file.ts
+++ packages/cli/src/lib/core/utils/file.ts
@@ -1,13 +1,12 @@
-import { extname, join, dirname } from 'path';
+import { join, dirname } from 'path';
 import { existsSync, readFileSync, writeFileSync, mkdirSync } from 'fs';
 import { logVerbose } from './loggin';
 import { getParserFromExtname, formatCode } from './prettier';
-import { throwError } from 'rxjs';
 
 /**
  * Ensures the file exists before reading it
  */
-export function readFile(path: string, fail: boolean = false): string {
+export function readFile(path: string, fail = false): string {
   const errorStr = `${path} does not exist.`;
   if (existsSync(path)) {
     return readFileSync(path, 'utf-8');
@@ -37,24 +36,26 @@ export function writeFile(filePath: string, data: string) {
   return writeFileSync(filePath, formattedData);
 }
 
-
-export function resolveAnyFile<T>(path: string): { exports: T, path: string } {
+export function resolveAnyFile<T>(path: string): { exports: T; path: string } {
   // start path from cwd
   path = join(process.cwd(), path);
-  let file;
 
   // 🔥 Live compilation of TypeScript files
   if (path.endsWith('.ts')) {
     // Register TS compiler lazily
     // tsNode needs the compilerOptions.module resolution to be 'commonjs',
     // so that imports in the `*.uf.ts` files work.
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('ts-node').register({
-      'compilerOptions': {
-        'module': 'commonjs'
-      }
+      transpileOnly: true,
+      compilerOptions: {
+        module: 'commonjs',
+        strict: false,
+      },
     });
   }
-  file = require(path);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const file = require(path);
 
   // If the user provides a configuration in TS file
   // then there are 2 cases for exporing an object. The first one is:
@@ -64,7 +65,6 @@ export function resolveAnyFile<T>(path: string): { exports: T, path: string } {
   const exports = file.default || file;
   return { exports, path };
 }
-
 
 /**
  * Upper or camelCase to lowercase hyphenated
@@ -76,5 +76,3 @@ export function toFileName(s: string): string {
     .replace(/[ _]/g, '-')
     .replace(/[/\\]/g, '-');
 }
-
-
