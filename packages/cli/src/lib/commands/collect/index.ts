9be3ca4cf9bed2cde33bdb2ccf511d1344db405d
Michael Hladky <michael@hladky.at>
2022-05-19 22:47:08 +0200

    refactor: execute all in run

--- packages/cli/src/lib/commands/collect/index.ts
+++ packages/cli/src/lib/commands/collect/index.ts
@@ -20,9 +20,11 @@ export const collectUserFlowsCommand: YargsCommandObject = {
       const potentialExistingCfg: RcJson = getCLIConfigFromArgv(argv as RcArgvOptions);
 
       await run([
-        setupRcJson
+        setupRcJson,
+        (cfg: RcJson) =>
+          startServerIfNeededAndExecute(() => collectReports(cfg), cfg.collect)
       ])(potentialExistingCfg);
-      await startServerIfNeededAndExecute(() => collectReports(potentialExistingCfg), potentialExistingCfg.collect)
+
     }
   }
 };
