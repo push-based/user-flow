c96c6d399d70fe51afb9ef6bf58cbaf101824b28
Michael Hladky <michael@hladky.at>
2022-08-17 00:10:47 +0200

    test: basic test for create reduced result

--- packages/cli/tests/functions/md-table.test.ts
+++ packages/cli/tests/functions/md-table.test.ts
@@ -1,9 +1,13 @@
 import * as LHR9 from '../data/lhr-9.json';
+import * as LHR9REDUCED from '../data/lhr-9_reduced.json';
 import * as LHR8 from '../data/lhr-8.json';
 import { userFlowReportToMdTable } from '../../src/lib/commands/assert/processes/md-table';
+import { reduce } from 'rxjs';
+import { createReducedReport } from '../../src/lib/commands/collect/processes/generate-reports';
 
 const lhr8 = LHR8 as any;
 const lhr9 = LHR9 as any;
+const lhr9reduced = LHR9REDUCED as any;
 
 describe('md-table', () => {
 
@@ -12,12 +16,13 @@ describe('md-table', () => {
     expect(parseFloat(lhr8.lhr.lighthouseVersion)).toBeLessThan(9);
   });
 
-  it('should NOT throw if version is greater than 9', () => {
+  it('should NOT throw if version is greater or equal than 9', () => {
     expect(parseFloat(lhr9.steps[0].lhr.lighthouseVersion)).toBeGreaterThan(9);
   });
 
-  it('should print MD table', () => {
-    expect(userFlowReportToMdTable(lhr9)[0]).toBe('ads');
+  it('should generate reduced JSON format for v9 raw JSON result', () => {
+    const reducedLhr9 = createReducedReport(lhr9);
+    expect(reducedLhr9).toEqual(lhr9reduced);
   });
 
 });
