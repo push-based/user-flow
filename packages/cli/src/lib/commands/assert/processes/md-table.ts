f178d443eb868dc98b873bfee1d3d38451fe28ae
Chris <chris@deep-blue.io>
2022-08-18 21:26:21 +0200

    fix: improve markdown table function

--- packages/cli/src/lib/commands/assert/processes/md-table.ts
+++ packages/cli/src/lib/commands/assert/processes/md-table.ts
@@ -1,9 +1,10 @@
+
 import { ReducedFlowStep, ReducedReport } from '../../collect/utils/user-flow/types';
 import {markdownTable} from 'markdown-table';
-import {}
+
 /**
  *
- * | Steps           | Performance | Accessibility | BestPractices | Seo  | PWA |
+ * | Step Name       | Gather Mode |Performance | Accessibility | BestPractices | Seo  | PWA |
  * | --------------- | ----------- | ------------- | ------------- | ---- | --- |
  * |  Nav1           |  99         | 50            | 100           | 98   |  -  |
  * |  Snap   1       |  3/3        | 22/5          | 5/2           | 7/10 |  -  |
@@ -13,8 +14,8 @@ export function userFlowReportToMdTable(
   reducedResult: ReducedReport
 ): string {
   const TABLE_OPTIONS = {align: 'c'}
-  const TABLE_HEAD = ['Steps', 'Performance', 'Accessibility', 'BestPractices', 'SEO', 'PWA'];
-  reducedResult.steps.map((step) => extractTableRow(step));
+  const TABLE_HEAD = ['Step Name', 'Gather Mode','Performance', 'Accessibility', 'BestPractices', 'SEO', 'PWA'];
+  //reducedResult.steps.map((step) => (extractTableRow(step)))
   const tableRow1 = ['Navigation report (127.0.0.1/)', '?', '100', '92', '100', '-'];
   const tableRow2 = ['Timespan report (127.0.0.1/)', '10/11', '-', '5/7', '-', '-'];
   const tableRow3 = ['Snapshot report (127.0.0.1/)', 'Ø 3/4', '10/10', '4/5', '9/9', '-'];
@@ -24,7 +25,10 @@ export function userFlowReportToMdTable(
 }
 
 function extractTableRow(step: ReducedFlowStep) {
-  if (step.type === "navigation") {
+  if (step.gatherMode === "navigation") {
+    
+  } else {
 
-  } 
+  }
+  return '';
 }
