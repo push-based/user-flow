04ab8cb2793218644cc1365155d5f8f9c10e404b
Chris <chris@deep-blue.io>
2022-08-19 23:37:17 +0200

    fix: add report format option of stdout

--- packages/cli/src/lib/commands/collect/constants.ts
+++ packages/cli/src/lib/commands/collect/constants.ts
@@ -2,5 +2,6 @@ export const REPORT_FORMAT_OPTIONS = [
   { name: 'HTML', value: 'html' },
   { name: 'JSON', value: 'json' },
   { name: 'Markdown', value: 'md' },
+  { name: 'Stdout', value: 'stdout'}
 ];
 export const REPORT_FORMAT_VALUES: string[] = REPORT_FORMAT_OPTIONS.map(v => v.value) as any as string[];
