444c2a3f09a6a892e8fcdd8b173ca02276d80e9d
Michael Hladky <michael@hladky.at>
2022-11-09 20:04:07 +0100

    fix: imports in interactive param

--- packages/cli/src/lib/global/options/interactive.ts
+++ packages/cli/src/lib/global/options/interactive.ts
@@ -1,8 +1,6 @@
 import { argv } from 'yargs';
 import { Param } from './interactive.model';
 import { ArgvOption } from '../../core/yargs/types';
-import { join } from "path";
-import { DEFAULT_RC_NAME, DEFAULT_RC_PATH } from './rc.constant';
 
 export const param: Param = {
   interactive: {
@@ -15,6 +13,6 @@ export const param: Param = {
 
 // We don't rely on yargs option normalization features as this can happen before cli bootstrap
 export function get(): boolean {
-    const {interactive, i } = argv as any as ArgvOption<any>;
-    return interactive !== undefined ?  Boolean(interactive) : i !== undefined ? Boolean(i) : param.interactive.default;
+  const { interactive, i } = argv as any as ArgvOption<any>;
+  return interactive !== undefined ? Boolean(interactive) : i !== undefined ? Boolean(i) : param.interactive.default;
 }
