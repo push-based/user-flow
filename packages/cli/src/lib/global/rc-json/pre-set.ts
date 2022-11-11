c9c33222dd952b4aad4d0dd0e783722722735209
Michael Hladky <michael@hladky.at>
2022-11-12 00:43:46 +0100

    refactor: add presets and tests

---
+++ packages/cli/src/lib/global/rc-json/pre-set.ts
@@ -0,0 +1,42 @@
+import { RcArgvOptions } from './types';
+import { GlobalOptionsArgv } from '../options/types';
+import { ArgvPreset } from '../../types';
+import { getCliMode } from '../cli-mode/cli-mode';
+
+export const DEFAULT_PRESET = {
+  // GLOBAL
+  interactive: true,
+  verbose: false,
+  // PERSIST COMMAND
+  openReport: true,
+  format: ['html']
+};
+
+export const CI_PRESET: Partial<GlobalOptionsArgv & RcArgvOptions> = {
+  // GLOBAL
+  interactive: false,
+  verbose: false,
+  // PERSIST COMMAND
+  openReport: false,
+  format: ['md', 'json']
+};
+
+export const SANDBOX_PRESET: ArgvPreset = {
+  // GLOBAL
+  verbose: true,
+  interactive: true,
+  // COLLECT COMMAND
+  openReport: false,
+  dryRun: true,
+};
+
+export function getEnvPreset(): ArgvPreset  {
+  const m = getCliMode();
+  if(m === 'SANDBOX') {
+    return SANDBOX_PRESET;
+  }
+  if(m === 'CI') {
+    return CI_PRESET;
+  }
+  return DEFAULT_PRESET;
+}
