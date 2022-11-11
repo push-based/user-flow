2bfc46553ee797df0fe625836265099f5adfefcf
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2022-11-11 20:52:55 +0100

    Update packages/cli/src/lib/global/options/interactive.ts

--- packages/cli/src/lib/global/options/interactive.ts
+++ packages/cli/src/lib/global/options/interactive.ts
@@ -14,5 +14,5 @@ export const param: Param = {
 // We don't rely on yargs option normalization features as this can happen before cli bootstrap
 export function get(): boolean {
   const { interactive, i } = argv as any as ArgvOption<any>;
-  return interactive !== undefined ? Boolean(interactive) : i !== undefined ? Boolean(i) : param.interactive.default;
+  return interactive !== undefined ? Boolean(interactive) : i !== undefined ? Boolean(i) : param.interactive.default;```
 }
