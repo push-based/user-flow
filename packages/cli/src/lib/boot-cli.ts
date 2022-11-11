702a6a1a6cef4fcc7eec7d2fca67dd9efc3d5edd
Michael Hladky <michael@hladky.at>
2022-11-11 21:57:29 +0100

    refactor: fix import

--- packages/cli/src/lib/boot-cli.ts
+++ packages/cli/src/lib/boot-cli.ts
@@ -1,11 +1,11 @@
 import { commands } from './commands';
 import { runCli } from './core/yargs';
 import { readRcConfig } from './global/rc-json';
-import { GLOBAL_OPTIONS } from './global/options';
+import { GLOBAL_OPTIONS_YARGS_CFG } from './global/options';
 
 function configParser(rcPath: string): {} {
   const {collect, persist, assert} = readRcConfig(rcPath);
   return {...collect, ...persist, ...assert};
 }
 
-(async () => runCli({ commands: commands, options: {...GLOBAL_OPTIONS}, config: configParser }))();
+(async () => runCli({ commands: commands, options: {...GLOBAL_OPTIONS_YARGS_CFG}, config: configParser }))();
