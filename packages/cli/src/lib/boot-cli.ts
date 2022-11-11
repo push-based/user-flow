6d18789f0c796b12810be7bc58c047b72470d71f
Michael Hladky <michael@hladky.at>
2022-11-11 23:09:29 +0100

    refactor: fix cfg

--- packages/cli/src/lib/boot-cli.ts
+++ packages/cli/src/lib/boot-cli.ts
@@ -10,7 +10,7 @@ function configParser(rcPath?: string): {} {
   let globalConfig: any = getCLIGlobalConfigFromArgv();
   // handle the global options of env specific presets
   const { interactive, verbose }: GlobalOptionsArgv = getEnvPreset();
-  return { ...globalConfig, ...rcConfig };
+  return { interactive, verbose, ...globalConfig, ...rcConfig };
 }
 
 (async () => runCli({
