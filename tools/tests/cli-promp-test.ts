25f78ba366ee9f67ba2e82b8ab9a8adeaf640c69
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2022-11-12 01:35:48 +0100

    Delete cli-promp-test.ts

--- tools/tests/cli-promp-test.ts
+++
@@ -1,9 +0,0 @@
-import * as _cliPromptTest from 'cli-prompts-test';
-import { CLI_MODE_PROPERTY } from '../../packages/cli/src/lib/cli-modes';
-
-export const cliPromptTest = (arr1: any[], arr2: any[], obj1: Object):
-  { exitCode: string, stdout: string, stderr: string } => {
-  (process.env as any)[CLI_MODE_PROPERTY] = 'SANDBOX';
-  return _cliPromptTest(arr1, arr2, obj1);
-};
-
