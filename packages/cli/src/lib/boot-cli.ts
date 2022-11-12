17902e775a50494177da82a6b6b21ac9992e8cb5
Michael Hladky <michael@hladky.at>
2022-11-12 01:50:21 +0100

    test: add global perets

--- packages/cli/src/lib/boot-cli.ts
+++ packages/cli/src/lib/boot-cli.ts
@@ -2,14 +2,19 @@ import { commands } from './commands';
 import { runCli } from './core/yargs';
 import { getCLIGlobalConfigFromArgv, getCliOptionsFromRcConfig } from './global/rc-json';
 import { GLOBAL_OPTIONS_YARGS_CFG } from './global/options';
+import { getEnvPreset } from './global/rc-json/pre-set';
+import { GlobalOptionsArgv } from './global/options/types';
 
 function configParser(rcPath?: string): {} {
   let rcConfig: any = getCliOptionsFromRcConfig(rcPath);
   let globalConfig: any = getCLIGlobalConfigFromArgv();
-  return { ...globalConfig, ...rcConfig };
+  // handle the global options of env specific presets
+  const { interactive, verbose }: GlobalOptionsArgv = getEnvPreset();
+  return { interactive, verbose, ...globalConfig, ...rcConfig };
 }
 
 (async () => runCli({
   commands: commands,
-  options: {...GLOBAL_OPTIONS_YARGS_CFG},
-  config: configParser }))();
+  options: { ...GLOBAL_OPTIONS_YARGS_CFG },
+  config: configParser
+}))();
