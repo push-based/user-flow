840e0c68d22dc94a7d34ba922ca9f49987f38d79
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2022-11-13 03:36:53 +0100

    Update packages/cli/src/lib/global/options/verbose.ts

--- packages/cli/src/lib/global/options/verbose.ts
+++ packages/cli/src/lib/global/options/verbose.ts
@@ -17,7 +17,7 @@ export const param: Param = {
 };
 
 // We don't rely on yargs option normalization features as this can happen before cli bootstrap
-export function get(): boolean | undefined {
+export function get(): boolean {
   const { verbose } = argv as any as ArgvOption<any>;
   return verbose !== undefined ? Boolean(verbose) : param.verbose.default;
 }
