366483b075af90cb321ca5bc74f611125d8394d3
Michael Hladky <michael@hladky.at>
2022-11-14 16:42:25 +0100

    test: impl logs for debigging

--- packages/cli/tests/functions/load-flow.test.ts
+++ packages/cli/tests/functions/load-flow.test.ts
@@ -43,6 +43,7 @@ describe('loading user-flow scripts for execution', () => {
     const ufPath = normalizePathForCi(invalidUfPath)
     const collectOptions = {url: 'example.com', ufPath};
     const userFlows = () => loadFlow(collectOptions);
+    expect(userFlows()).toBe([]);
     expect(userFlows).toThrow(`ufPath: ${join(process.cwd(), ufPath)} is no directory`);
   });
 
