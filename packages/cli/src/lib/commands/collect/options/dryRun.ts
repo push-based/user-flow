5a0eb02fc7c81a95536a01bfd8e7e7bdba39269d
Michael Hladky <michael@hladky.at>
2022-11-14 23:28:09 +0100

    refactor: move default handling into params

--- packages/cli/src/lib/commands/collect/options/dryRun.ts
+++ packages/cli/src/lib/commands/collect/options/dryRun.ts
@@ -1,16 +1,17 @@
 import { argv } from 'yargs';
 import { Param } from './dryRun.model';
 import { ArgvOption } from '../../../core/yargs/types';
+import { getEnvPreset } from '../../../global/rc-json/pre-set';
 
 export const param: Param = {
   dryRun: {
     type: 'boolean',
     description: 'Execute commands without effects',
-    default: false
+    default: getEnvPreset().dryRun
   }
 };
 
 export function get(): boolean {
   const { dryRun } = argv as any as ArgvOption<Param>;
-  return dryRun;
+  return dryRun !== undefined ? dryRun : param.dryRun.default;
 }
