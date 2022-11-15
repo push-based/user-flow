0835b47e756220f179c9a2861c876b29e08434a1
Michael Hladky <michael@hladky.at>
2022-11-15 13:30:53 +0100

    refactor: add cli tests

---
+++ packages/cli/tests/cli/cli-configuration.test.ts
@@ -0,0 +1,117 @@
+import { cliPromptTest } from '../utils/cli-prompt-test/cli-prompt-test';
+import { CLI_PATH } from '../fixtures/cli-bin-path';
+import {
+  resetSetupSandboxAndKillPorts,
+  SETUP_SANDBOX_CLI_TEST_CFG,
+  SETUP_SANDBOX_DEFAULT_RC_JSON, SETUP_SANDBOX_DEFAULT_RC_NAME,
+  SETUP_SANDBOX_PATH
+} from '../fixtures/setup-sandbox';
+import { EMPTY_SANDBOX_CLI_TEST_CFG, resetEmptySandbox } from '../fixtures/empty-sandbox';
+import { expectCfgToContain } from '../utils/cli-expectations';
+import * as path from 'path';
+import { RcArgvOptions } from '@push-based/user-flow';
+import { GlobalOptionsArgv } from '../../src/lib/global/options/types';
+import { getEnvPreset } from '../../src/lib/global/rc-json/pre-set';
+import { AssertOptions, CollectOptions, PersistOptions } from '../../src/lib/global/rc-json/types';
+
+const initCommand = [CLI_PATH, 'init'];
+const collectCommand = [CLI_PATH, 'collect'];
+
+describe('the CLI configurations', () => {
+  beforeEach(async () => {
+    resetEmptySandbox();
+    resetSetupSandboxAndKillPorts();
+  });
+  afterEach(async () => {
+    resetEmptySandbox();
+    resetSetupSandboxAndKillPorts();
+  });
+
+  it('should have default`s configured in a fresh environment', async () => {
+    const { exitCode, stdout, stderr } = await cliPromptTest(
+      [
+        ...initCommand
+      ],
+      [],
+      EMPTY_SANDBOX_CLI_TEST_CFG
+    );
+    //expect(stdout).toBe('');
+    const cfg: Partial<GlobalOptionsArgv & RcArgvOptions> = getEnvPreset();
+    // expectCfgToContain(stdout, cfg);
+    expect(stderr).toBe('');
+    expect(exitCode).toBe(0);
+  });
+
+  it('should read the existing .rc configuration', async () => {
+    const { exitCode, stdout, stderr } = await cliPromptTest(
+      [
+        ...collectCommand
+      ],
+      [],
+      SETUP_SANDBOX_CLI_TEST_CFG
+    );
+    const { collect, persist, assert } = SETUP_SANDBOX_DEFAULT_RC_JSON;
+    const cfg = { ...collect, ...persist, ...assert };
+    expectCfgToContain(stdout, cfg);
+    expect(stderr).toBe('');
+    expect(exitCode).toBe(0);
+  });
+
+  it('should accept CLI params as configuration', async () => {
+    const globalOptions: GlobalOptionsArgv = {
+      verbose: true,
+      interactive: true,
+      rcPath: path.join(SETUP_SANDBOX_PATH, SETUP_SANDBOX_DEFAULT_RC_NAME)
+    };
+
+    const collect: CollectOptions = {
+      url: 'xxx',
+      ufPath: 'xxx',
+      serveCommand: 'xxx',
+      awaitServeStdout: 'xxx',
+      dryRun: false
+    };
+
+    const persist: PersistOptions = {
+      outPath: 'xxx',
+      format: ['json', 'md'],
+      openReport: true
+    } as PersistOptions;
+
+    const assert: AssertOptions = {};
+
+    const { verbose, interactive, rcPath } = globalOptions;
+    const { url, ufPath, serveCommand, awaitServeStdout, dryRun } = collect;
+    let { outPath, format, openReport } = persist;
+
+    const { exitCode, stdout, stderr } = await cliPromptTest(
+      [
+        ...initCommand,
+        // global
+        `--rcPath=${rcPath}`,
+        `--interactive=${interactive}`,
+        `--verbose=${verbose}`,
+        // collect
+        `--url=${url}`,
+        `--ufPath=${ufPath}`,
+        `--dryRun=${dryRun}`,
+        `--serveCommand=${serveCommand}`,
+        `--awaitServeStdout=${awaitServeStdout}`,
+        // persist
+        `--outPath=${outPath}`,
+        `--format=${format[0]}`,
+        `--format=${format[1]}`,
+        `--openReport=${openReport}`
+        // assert
+      ],
+      ['n'],
+      SETUP_SANDBOX_CLI_TEST_CFG
+    );
+
+    const cfg = { ...collect, ...persist, ...assert };
+    expectCfgToContain(stdout, cfg);
+    expect(stderr).toBe('');
+    expect(exitCode).toBe(0);
+  });
+
+});
--- packages/cli/tests/utils/cli-expectations.ts
+++ packages/cli/tests/utils/cli-expectations.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import {join} from 'path';
+import { join } from 'path';
 import { RcJson } from '@push-based/user-flow';
 import FlowResult from 'lighthouse/types/lhr/flow';
 import Budget from 'lighthouse/types/lhr/budget';
@@ -8,6 +8,10 @@ import { PROMPT_COLLECT_UF_PATH } from '../../src/lib/commands/collect/options/u
 import { PROMPT_COLLECT_URL } from '../../src/lib/commands/collect/options/url.constant';
 import { PROMPT_PERSIST_OUT_PATH } from '../../src/lib/commands/collect/options/outPath.constant';
 import { SETUP_CONFIRM_MESSAGE } from '../../src/lib/commands/init/constants';
+import { ArgvOption } from '../../src/lib/core/yargs/types';
+import { GlobalOptionsArgv } from '../../src/lib/global/options/types';
+import { RcArgvOptions } from '../../src/lib';
+import { getCliOptionsFromRcConfig } from '../../src/lib/global/rc-json';
 
 export function expectOutputRcInStdout(stdout: string, cfg: RcJson) {
   expect(stdout).toContain(SETUP_CONFIRM_MESSAGE);
@@ -41,10 +45,10 @@ export function expectNoBudgetsFileExistLog(stdout: string) {
 
 export function expectResultsToIncludeBudgets(resultPath: string, budgets: Budget[] | string) {
   let resolvedBudgets: Budget[];
-  if(Array.isArray(budgets)) {
+  if (Array.isArray(budgets)) {
     resolvedBudgets = budgets;
   } else {
-    expect(() => fs.readFileSync(budgets+'')).not.toThrow();
+    expect(() => fs.readFileSync(budgets + '')).not.toThrow();
     resolvedBudgets = JSON.parse(fs.readFileSync(budgets) as any) as Budget[];
   }
 
@@ -88,6 +92,37 @@ export function expectCollectCreatesHtmlReport(reportPath: string, ufName: strin
   expect(reportHTML).toBeTruthy();
 }
 
+export function expectCfgToContain(stdout: string, cliParams: {}) {
+
+  Object.entries(cliParams).forEach(([k, v]) => {
+    switch (k) {
+      // global
+      case 'verbose':
+      case 'interactive':
+        expect(stdout).toContain(`${k}: ${v}`);
+        break;
+      // global
+      case 'rcPath':
+      // collect
+      case 'url':
+      case 'ufPath':
+      case 'outPath':
+      case 'dryRun':
+        expect(stdout).toContain(`${k}: '${v}'`);
+        break;
+      case 'format':
+        expect(stdout).toContain(`${k}: [ ${(v as any[]).map(i => "'"+i+"'").join(', ')} ]`);
+        break;
+      case 'openReport':
+        expect(stdout).toContain(`${k}: ${v}`);
+        break;
+      default:
+        throw new Error(`${k} handling not implemented`)
+        break;
+    }
+  });
+}
+
 export function expectCollectCreatesJsonReport(reportPath: string, ufName: string) {
   let reportJson;
   expect(() => fs.readFileSync(reportPath)).not.toThrow();
@@ -118,7 +153,7 @@ export function expectCollectNotToCreateAReport(reportPath: string) {
   }
 }
 
-export function expectEnsureConfigToCreateRc(rcPath: string, cfg: RcJson) {
+export function expectEnsureConfigToCreateRc(rcPath: string, cfg: Partial<RcJson>) {
   expect(() => fs.readFileSync(rcPath)).not.toThrow();
   const config = JSON.parse(fs.readFileSync(rcPath) as any);
   // handle inconsistency of rc vs params
