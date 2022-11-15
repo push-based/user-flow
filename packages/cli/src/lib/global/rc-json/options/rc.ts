c876b8b79381913a4b6fe62299d78594bb3fdd0c
Michael Hladky <michael@hladky.at>
2022-11-15 02:26:33 +0100

    refactor: adopt default handling in option getter for rcPath

--- packages/cli/src/lib/global/rc-json/options/rc.ts
+++ packages/cli/src/lib/global/rc-json/options/rc.ts
@@ -1,25 +1,19 @@
 import { argv } from 'yargs';
 import { Param } from './rc.model';
-import { ArgvOption } from '../../../core/yargs/types';
-import { join } from 'path';
-import { DEFAULT_RC_NAME, DEFAULT_RC_PATH } from './rc.constant';
 import { getEnvPreset } from '../pre-set';
+import { GlobalOptionsArgv } from '../../options/types';
 
-function getDefaultByCliMode(): string {
-  return getEnvPreset().rcPath as string;
-}
 export const param: Param = {
   rcPath: {
     alias: 'p',
     type: 'string',
     description: 'Path to user-flow.config.json. e.g. `./user-flowrc.json`',
-    default: getDefaultByCliMode()
+    default: getEnvPreset().rcPath as string
   }
 };
 
 // We don't rely on yargs option normalization features as this can happen before cli bootstrap
 export function get(): string {
-  const { rcPath, p, ['rc-path']: rc_path } = argv as any as ArgvOption<any>;
-  const pathToCfgRc = rcPath || rc_path || p;
-  return (pathToCfgRc as any as string) || param.rcPath.default;
+  const { rcPath } = argv as any as GlobalOptionsArgv;
+  return rcPath as string;
 }
