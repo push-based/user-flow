61aeabf3bdc00f22b2c0e9e11c9a703bb86641ae
Michael Hladky <michael@hladky.at>
2022-11-15 12:44:07 +0100

    refactor: adopt openReport defaults

--- packages/cli/src/lib/commands/collect/options/openReport.ts
+++ packages/cli/src/lib/commands/collect/options/openReport.ts
@@ -3,22 +3,17 @@ import { Param } from './openReport.model';
 import { ArgvOption } from '../../../core/yargs/types';
 import { getEnvPreset } from '../../../global/rc-json/pre-set';
 
-function getDefaultByCliMode(): boolean {
-  return getEnvPreset().openReport;
-}
-
-
 export const param: Param = {
   openReport: {
     alias: 'e',
     type: 'boolean',
     description: 'Opens browser automatically after the user-flow is collected. (true by default)',
-    default: getDefaultByCliMode(),
+    default: getEnvPreset().openReport,
     requiresArg: true
   }
 };
 
 export function get(): boolean {
   const {  openReport } = argv as any as ArgvOption<Param>;
-  return openReport !== undefined ? Boolean(openReport) : param.openReport.default
+  return openReport;
 }
