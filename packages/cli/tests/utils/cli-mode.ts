1952fe9abcabc659adcd05fc15d1f6952091910a
Michael Hladky <michael@hladky.at>
2022-11-16 13:56:49 +0100

    test: adjust setupEnv

--- packages/cli/tests/utils/cli-mode.ts
+++ packages/cli/tests/utils/cli-mode.ts
@@ -1,7 +1,11 @@
 import { CI_PROPERTY, CLI_MODE_PROPERTY } from '../../src/lib/global/cli-mode/cli-mode';
 
-export function setupEnvVars(ciPropVal: string) {
-  process.env[CI_PROPERTY] = ciPropVal;
+export function setupEnvVars(env: CLI_MODES): void {
+  if (env === 'DEFAULT') {
+    delete process.env[CI_PROPERTY];
+  } else {
+    process.env[CI_PROPERTY] = (env === 'CI' ? true : 'SANDBOX') as string;
+  }
 }
 
 export function teardownEnvVars() {
