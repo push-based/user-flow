0f42b8c30241ef2d17303744a6b3e147f8e6d99a
Michael Hladky <michael@hladky.at>
2022-11-18 19:32:31 +0100

    refactor: adopt rc parsing

--- packages/cli/src/lib/global/rc-json/index.ts
+++ packages/cli/src/lib/global/rc-json/index.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from '../../core/file';
 import { logVerbose } from '../../core/loggin';
-import { CollectOptions, PersistOptions, RcArgvOptions, RcJson } from './types';
+import { AssertOptions, CollectOptions, PersistOptions, RcArgvOptions, RcJson } from './types';
 import { GlobalOptionsArgv } from '../options/types';
 import { get as getRcParam, get as getRcPath } from './options/rc';
 import { get as getVerbose } from '../options/verbose';
@@ -40,29 +40,26 @@ export function getCLIGlobalConfigFromArgv(): GlobalOptionsArgv {
 }
 
 export function getCLIConfigFromArgv(argv: RcArgvOptions): RcJson {
-  const { url, ufPath, serveCommand, awaitServeStdout, outPath, format, budgetPath, budgets, openReport, dryRun } = (argv || {}) as any as (keyof CollectOptions & keyof PersistOptions);
+  // @TODO derive from commands josn spec
+  const {
+    url, ufPath, serveCommand, awaitServeStdout, dryRun, openReport,
+    outPath, format, budgetPath, budgets } = (argv || {}) as any as (keyof CollectOptions & keyof PersistOptions);
 
-  const cfg: RcJson = {
-    collect: {
-      url,
-      ufPath,
-      serveCommand,
-      awaitServeStdout,
-      dryRun,
-      openReport
-    },
-    persist: {
-      outPath,
-      format,
-    }
-  };
+  let collect = {} as CollectOptions;
+  url && (collect.url = url);
+  ufPath && (collect.ufPath = ufPath);
+  serveCommand && (collect.serveCommand = serveCommand);
+  awaitServeStdout && (collect.awaitServeStdout = awaitServeStdout);
+  dryRun && (collect.dryRun = dryRun);
+  openReport && (collect.openReport = openReport);
 
-  if (budgetPath || budgets) {
-    cfg.assert = {
-      budgetPath,
-      budgets
-    };
-  }
+  let persist = {} as PersistOptions;
+  outPath && (persist.outPath = outPath);
+  format && (persist.format = format);
 
-  return cfg;
+  let assert = {} as AssertOptions;
+  budgetPath && (assert.budgetPath = budgets);
+  budgets && (assert.budgets = budgets);
+
+  return { collect, persist, assert };
 }
