df96ff9af848faa95dc82fd39777b28618d6044f
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2022-11-20 00:01:42 +0100

    Update packages/cli/src/lib/commands/collect/utils/user-flow/open-report.ts

--- packages/cli/src/lib/commands/collect/utils/user-flow/open-report.ts
+++ packages/cli/src/lib/commands/collect/utils/user-flow/open-report.ts
@@ -6,7 +6,7 @@ import * as openFileInBrowser from 'open';
 
 export async function openFlowReport(fileNames: string[]): Promise<void> {
   // open report if requested and not in executed in CI
-  if (openOpt()) {
+if (!dryRun() && openOpt() && interactive()) {
 
     const htmlReport = fileNames.find(i => i.includes('.html'));
     if (htmlReport) {
