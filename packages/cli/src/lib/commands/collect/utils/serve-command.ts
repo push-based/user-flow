0065a5e20bd0929ff3bb9f044fdbec6de53abb58
Chris <chris@deep-blue.io>
2022-11-19 13:14:52 +0100

    fix: fix typo in comment

--- packages/cli/src/lib/commands/collect/utils/serve-command.ts
+++ packages/cli/src/lib/commands/collect/utils/serve-command.ts
@@ -5,7 +5,7 @@ import { Subscription } from 'rxjs';
 
 // @TODO as it is quite har to maintain and test the serve command we have to think about a better way to wrap it
 // I suggest a single function returning a promise.
-// This fn takes the serve options as well ans the run block and makes shure execution is done correctly and errors are forwarded too.
+// This fn takes the serve options as well ans the run block and makes sure execution is done correctly and errors are forwarded too.
 // In there we compose easier to test fn's
 
 export async function startServerIfNeededAndExecute(workTargetingServer: () => Promise<any>, collectOption: CollectOptions = {} as CollectOptions): Promise<RcJson> {
