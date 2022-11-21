5c9c4e49aba2a3543e6ac180a83fdb22dbf6bf42
Michael Hladky <michael@hladky.at>
2022-11-21 01:29:45 +0100

    test: implement cli wrapper PoC3

--- packages/cli/tests/utils/cli-prompt-test/cli-prompt-test.ts
+++ packages/cli/tests/utils/cli-prompt-test/cli-prompt-test.ts
@@ -1,116 +1,26 @@
-import { cliPromptTest as _cliPromptTest, PromptTestOptions } from './raw';
-import { ExecaChildProcess, Options } from 'execa';
+import * as _cliPromptTest from 'cli-prompts-test';
 import { CI_PROPERTY } from '../../../src/lib/global/cli-mode/cli-mode';
 import { CLI_MODES } from '../../../src/lib/global/cli-mode/types';
 
-
-export type ProcessParams = {
-  // command placeholder
-  _ : string,
-  [key as string ]: string | string[]
-}
-
-function processParamsToParamsArray(params: ProcessParams): string[] {
-  Object.entries(params).map(([key, value]) => {
-    if(key === '_') {
-      return value;
-    } else if(Array.isArray(value)) {
-      return value.map(v => `--${k}=${v}`);
-    } else {
-      return `--${k}=${v}`;
-    }
-  });
-}
-
-export type CliProcess = {
-  exec: (processParams: string[], userInput: string[]) => Promise<ExecaChildProcess>
-}
-type ExecFn = (processParams: ProcessParams, userInput: string[], promptOptions: PromptTestOptions) => Promise<ExecaChildProcess>;
 /**
- *
- * @param options: passed directly to execa as options
- */
-export function getCliProcess(options: Options): CliProcess {
-  return {
-    exec: (processParams: string[], userInput: string[], promptOptions: PromptTestOptions = {}): Promise<ExecaChildProcess>  => {
-      return _cliPromptTest(processParams, userInput, options, promptOptions);
-    }
-  };
-}
-
-function handleCliModeEnvVars(cliMode: CLI_MODES): string {
-  let ciValue: string = '';
-  if (cliMode === 'DEFAULT') {
-    delete process.env[CI_PROPERTY];
-  } else if (cliMode === 'SANDBOX') {
-    // emulate sandbox env by setting CI to SANDBOX
-    ciValue = 'SANDBOX';
-  }
-  // CI mode
-  else {
-    ciValue = 'true';
-  }
-  return ciValue;
-}
-
-
-export type Project = {
-  root: string,
-  readFile: () => string,
-  exec: ExecFn
-}
-
-type ProjectConfig = {
-  root: string,
-  cliMode: CLI_MODES,
-  execName: string
-  // the process env of the created process
-  env: Record<string, string>,
-}
-
-export function setupProject(cfg: ProjectConfig): Project {
-  const process = getCliProcess({
-    cwd: cfg.root
-  });
-
-  return {
-    root: cfg.root,
-    exec: (processParams, userInput, promptOptions) => {
-      process.exec()
-    }
-  } as Project;
-}
-
-/**
- * @param {string[]} processParams CLI args to pass in
- * @param {string[]} userInput answers to be passed to stdout
+ * @param {string[]} args CLI args to pass in
+ * @param {string[]} answers answers to be passed to stdout
  * @param {Object} [options] specify the testPath and timeout
  *
  * returns {Promise<Object>}
  */
-export function cliPromptTest(processParams: string[], userInput: string[], options: Options, cliMode: CLI_MODES = 'SANDBOX'): Promise<ExecaChildProcess<string>> {
-  let opt = {...options};
-
-  let ciValue: string = handleCliModeEnvVars(cliMode);
-  if (ciValue) {
-    opt['env'] = {
-      [CI_PROPERTY]: ciValue
-    };
+export function cliPromptTest(args, answers, options, cliMode?: CLI_MODES) {
+  const _cliMode: CLI_MODES = cliMode || 'SANDBOX';
+  if(_cliMode === 'DEFAULT') {
+    delete process.env[CI_PROPERTY];
   }
-  const cli = getCliProcess(opt);
-  processParamsToParamsArray()
-  return cli.exec(processParams, userInput);
-}
-
-export function promptTest(processParams: ProcessParams, userInput: string[], options: Options, cliMode: CLI_MODES = 'SANDBOX'): Promise<ExecaChildProcess<string>> {
-  let opt = {...options};
-
-  let ciValue: string = handleCliModeEnvVars(cliMode);
-  if (ciValue) {
-    opt['env'] = {
-      [CI_PROPERTY]: ciValue
-    };
+  else if(_cliMode === 'SANDBOX') {
+    // emulate sandbox env by setting CI to SANDBOX
+    process.env[CI_PROPERTY] = 'SANDBOX';
   }
-  const cli = getCliProcess(opt);
-  return cli.exec(processParamsToParamsArray(processParams), userInput);
+  // CI mode
+  else {
+    process.env[CI_PROPERTY] = 'true';
+  }
+  return _cliPromptTest(args, answers, options);
 }
