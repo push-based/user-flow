2cba0c999ec6e5f2e5149d690458615ee0c50096
Michael Hladky <michael@hladky.at>
2022-11-22 01:46:04 +0100

    test: fix collect tests

--- packages/cli/tests/commands/collect/collect.test.ts
+++ packages/cli/tests/commands/collect/collect.test.ts
@@ -87,13 +87,13 @@ describe('collect command in setup sandbox', () => {
   }, 90_000);
 
   it('should load ufPath, execute the user-flow on a remote URL and save the results as a JSON file', async () => {
-    // const r = await setupPrj.$collect({ rcPath: SETUP_SANDBOX_REMOTE_RC_NAME });
-    // @TODO remove old test script
+    const { exitCode, stdout, stderr } = await setupPrj.$collect({ rcPath: SETUP_SANDBOX_REMOTE_RC_NAME });
+    /*// @TODO remove old test script
     const { exitCode, stdout, stderr } = await cliPromptTest(
       [...collectCommandRemoteRc],
       [],
       SETUP_SANDBOX_CLI_TEST_CFG
-    );
+    );*/
     expect(stderr).toBe('');
     // expect(stdout).toBe('');
     // expectCollectLogsFromMockInStdout(stdout, uf1Name, SETUP_SANDBOX_REMOTE_RC_JSON);
