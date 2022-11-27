9e17a74b290facef35a2aaee350501904b834310
Michael Hladky <michael@hladky.at>
2022-11-27 18:45:17 +0100

    test: fix test helper 4

--- packages/cli/src/lib/commands/collect/options/format.constant.ts
+++ packages/cli/src/lib/commands/collect/options/format.constant.ts
@@ -1,10 +1,12 @@
-import { REPORT_FORMAT_OPTIONS } from '../constants';
+import { REPORT_FORMAT_VALUES } from '../constants';
 import { ReportFormat } from './types';
 
 export const DEFAULT_PERSIST_FORMAT: ReportFormat[] = ['html'];
 export const PROMPT_PERSIST_FORMAT = 'What is the format of user-flow reports? (use ⬇/⬆ to navigate, and SPACE key to select)';
 export const ERROR_PERSIST_FORMAT_REQUIRED = 'format is required. Either through the console as `--format` or in the `.user-flow.json`';
 export const ERROR_PERSIST_FORMAT_WRONG = (wrongFormat: string) => {
-  return `Wrong format: "${wrongFormat}". Format has to be one of: ${REPORT_FORMAT_OPTIONS.map(f => f.value).join(', ')}`;
-}
+  return `Argument: format, Given: "${wrongFormat}", Choices: ${REPORT_FORMAT_VALUES.map(v => '"' + v + '"').join(', ')}`;
+  // @TODO decide for yergs error handling od custom for choices
+  // return `Wrong format: "${wrongFormat}". Format has to be one of: ${REPORT_FORMAT_OPTIONS.map(f => f.value ).join(', ')}`;
+};
 
