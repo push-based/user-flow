e54d6ebca06f363620c42bb1e6824c3ff5d00883
Chris <chris@deep-blue.io>
2022-11-30 23:16:03 +0100

    fix: add favicon and img to static project

--- packages/cli/tests/fixtures/sandbox/static.ts
+++ packages/cli/tests/fixtures/sandbox/static.ts
@@ -3,6 +3,8 @@ import { DEFAULT_RC_NAME } from '../../../src/lib/constants';
 import { STATIC_RC_JSON, STATIC_USERFLOW_PATH } from '../rc-files/static';
 import { STATIC_USERFLOW_CONTENT } from '../user-flows/static.uf';
 import { INITIATED_PRJ_BIN, INITIATED_PRJ_ROOT } from './initiated';
+import { STATIC_APP_FAVICON_NAME, STATIC_APP_FAVICON_CONTENT } from "../static-app/favico";
+import { STATIC_APP_IMG_CONTENT, STATIC_APP_IMG_NAME } from "../static-app/user-flow-square";
 
 // @TODO add static-app files to serve
 export const STATIC_PRJ_CFG: UserFlowProjectConfig = {
@@ -14,6 +16,8 @@ export const STATIC_PRJ_CFG: UserFlowProjectConfig = {
   create: {
     [STATIC_RC_JSON.collect.ufPath]: undefined,
     [STATIC_RC_JSON.persist.outPath]: undefined,
-    [STATIC_USERFLOW_PATH]: STATIC_USERFLOW_CONTENT
+    [STATIC_USERFLOW_PATH]: STATIC_USERFLOW_CONTENT,
+    [STATIC_APP_FAVICON_NAME]: STATIC_APP_FAVICON_CONTENT,
+    [STATIC_APP_IMG_NAME]: STATIC_APP_IMG_CONTENT,
   }
 };
