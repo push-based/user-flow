13b43748774a6769085e8ef933c11ec10b09cc85
Chris <chris@deep-blue.io>
2022-11-30 21:58:25 +0100

    fix: upgrade jest transform config

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -2,13 +2,8 @@
 export default {
   displayName: 'cli',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': ['ts-jest', '<rootDir>/tsconfig.spec.json'],
   },
   transformIgnorePatterns: ['/node_modules/(?!markdown-table/.*)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
