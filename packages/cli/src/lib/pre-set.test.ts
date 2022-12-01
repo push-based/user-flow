6445872394690f9b72ca64c5937833b9d51d11fd
Michael Hladky <michael@hladky.at>
2022-12-01 03:44:02 +0100

    refactor: fix broken imports

--- packages/cli/src/lib/pre-set.test.ts
+++ packages/cli/src/lib/pre-set.test.ts
@@ -1,9 +1,40 @@
 import { CI_PRESET, DEFAULT_PRESET, getEnvPreset, SANDBOX_PRESET } from './pre-set';
-import { setupEnvVars, teardownEnvVars } from '../../tests/utils/cli-mode';
+import { UserFlowCliProject, UserFlowCliProjectFactory, UserFlowProjectConfig } from '../../user-flow-testing';
+import { EMPTY_PRJ_CFG } from '../../test-data/empty-prj';
+let emptyPrjSandbox: UserFlowCliProject;
+
+import { CI_PROPERTY, CLI_MODE_PROPERTY } from '../../src/lib/global/cli-mode/cli-mode';
+import { CLI_MODES } from '../../src/lib/global/cli-mode/types';
+
+function setupEnvVars(env: CLI_MODES): void {
+  if (env === 'DEFAULT') {
+    delete process.env[CI_PROPERTY];
+  } else {
+    process.env[CI_PROPERTY] = (env === 'CI' ? true : 'SANDBOX') as string;
+  }
+}
+
+function teardownEnvVars() {
+  delete process.env[CI_PROPERTY];
+  delete process.env[CLI_MODE_PROPERTY];
+}
+
+const emptyPrjDefaultCfg: UserFlowProjectConfig = {
+  ...EMPTY_PRJ_CFG,
+  cliMode: 'DEFAULT'
+};
+let emptyPrjDefault: UserFlowCliProject;
 
 describe('getEnvPreset', () => {
 
-  afterEach(() => setupEnvVars('SANDBOX'));
+  afterEach(async () => {
+    if (!emptyPrjSandbox) {
+      emptyPrjSandbox = await UserFlowCliProjectFactory.create(EMPTY_PRJ_CFG);
+    }
+    await emptyPrjSandbox.setup();
+    process.chdir(emptyPrjSandbox.root);
+    setupEnvVars('SANDBOX')
+  });
   afterEach(teardownEnvVars);
 
   it('should return default preset', () => {
