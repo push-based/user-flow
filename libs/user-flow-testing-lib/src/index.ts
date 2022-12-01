c0acfd4d5ba5bcb4968e60b9c4b1c627a7a9b9c1
Michael Hladky <michael@hladky.at>
2022-12-01 01:53:23 +0100

    refactor: reduce exports

--- libs/user-flow-testing-lib/src/index.ts
+++ libs/user-flow-testing-lib/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/data/user-flowrc.base';
-export * from './lib/utils/kill';
 export {SERVE_COMMAND_PORT} from './lib/constants';
 export * from './lib/expect';
 export {UserFlowProjectConfig} from './lib/types';
