94fbe314e59241bb92dd7812b139cea8c1ade088
Michael Hladky <michael@hladky.at>
2022-12-01 03:29:03 +0100

    refactor: remove old lib

--- libs/user-flow-testing-lib/src/index.ts
+++
@@ -1,5 +0,0 @@
-export const t = '';
-//export {SERVE_COMMAND_PORT} from './lib/constants';
-//export * from './lib/expect';
-//export {UserFlowProjectConfig} from './lib/types';
-//export {UserFlowCliProject, UserFlowCliProjectFactory} from './lib/user-flow-cli';
