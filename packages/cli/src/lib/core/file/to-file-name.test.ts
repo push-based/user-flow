e175a440494a0ffa071f7deccb4d6d18e63927d6
Michael Hladky <michael@hladky.at>
2022-12-17 12:05:41 +0100

    test: add more unit tests for toFileName fn

--- packages/cli/src/lib/core/file/to-file-name.test.ts
+++ packages/cli/src/lib/core/file/to-file-name.test.ts
@@ -14,15 +14,19 @@ describe('toFileName', () => {
   it('should escape a URL and port', () => {
     const url = 'www.test.com';
     const urlAndPort = `https://${url}:4200`;
-    expect(toFileName(urlAndPort)).toEqual(url+'-'+4200);
+    expect(toFileName(urlAndPort)).toEqual(url + '-' + 4200);
   });
 
 
   it('should escape a folder name', () => {
     const folder = 'my-folder-name';
     const folder2 = 'myFolderName';
+    const folder3 = 'my folder name';
+    const folder4 = 'my Folder Name';
     expect(toFileName(folder)).toEqual(folder);
     expect(toFileName(folder2)).toEqual(folder);
+    expect(toFileName(folder3)).toEqual(folder);
+    expect(toFileName(folder4)).toEqual(folder);
   });
 
 });
