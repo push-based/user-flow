fe9e5cc07abb438758b70a48c7857fb26b7069ab
Michael Hladky <michael@hladky.at>
2022-12-18 15:01:55 +0100

    fix: wip2

--- packages/cli-testing/jest.config.ts
+++ packages/cli-testing/jest.config.ts
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/packages/cli-testing',
+  coverageDirectory: '../../../coverage/packages/cli-testing',
 };
