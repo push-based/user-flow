79e0d091c69c8bd11915a7e1341b90804f4672c1
Michael Hladky <michael@hladky.at>
2022-12-21 20:38:00 +0100

    fix: imports in user-flow cli test

--- packages/user-flow-cli-testing/src/lib/types.ts
+++ packages/user-flow-cli-testing/src/lib/types.ts
@@ -1,4 +1,4 @@
-import { ExecFn, Project, ProjectConfig } from '@push-based/cli-testing/cli-project';
+import { ExecFn, Project, ProjectConfig } from '@push-based/cli-testing';
 import { CLI_MODES, CollectCommandArgv, GlobalOptionsArgv, InitCommandArgv, RcJson } from '@push-based/user-flow';
 
 export type UserFlowProject = Project & {
