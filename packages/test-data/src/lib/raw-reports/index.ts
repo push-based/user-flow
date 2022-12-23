97b3028416cbfaeae4763c7b20169a48df323e0c
Michael Hladky <michael@hladky.at>
2022-12-24 00:48:18 +0100

    test: fix imports

--- packages/test-data/src/lib/raw-reports/index.ts
+++ packages/test-data/src/lib/raw-reports/index.ts
@@ -3,7 +3,8 @@ import { readFileSync } from 'fs';
 
 export function getReportContent<T = string>(fileName: string): T {
   const path = join(__dirname, fileName);
-  const report: string = readFileSync(path, 'utf-8');
+  let report: string = readFileSync(path, 'utf-8').trim();
+  report = report[1] === ' ' ? '| ' + report.slice(2) : report;
   if (fileName.endsWith('.json')) {
     return JSON.parse(report as string);
   }
