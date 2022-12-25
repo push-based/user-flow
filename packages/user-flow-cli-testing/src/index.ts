0c4cc5fd7466cca781c25d5f451e363c31c2f3a8
Michael Hladky <michael@hladky.at>
2022-12-25 01:33:31 +0100

    test: adopt exports

--- packages/user-flow-cli-testing/src/index.ts
+++ packages/user-flow-cli-testing/src/index.ts
@@ -1,3 +1,10 @@
+export {
+  DEFAULT_COLLECT_UF_PATH,
+  DEFAULT_COLLECT_URL,
+  DEFAULT_PERSIST_OUT_PATH,
+  getEnvPreset
+} from '@push-based/user-flow';
+export {ENTER, DOWN, SPACE} from '@push-based/cli-testing';
 export * from './lib/constants';
 export * from './lib/types';
 export * from './lib/utils/cli-mode';
--- packages/user-flow-cli-testing/src/lib/types.ts
+++ packages/user-flow-cli-testing/src/lib/types.ts
@@ -1,6 +1,15 @@
 import { ExecFn, Project, ProjectConfig } from '@push-based/cli-testing';
 import { CLI_MODES, CollectCommandArgv, GlobalOptionsArgv, InitCommandArgv, RcJson } from '@push-based/user-flow';
 
+export {
+  RcJson,
+  UserFlowInteractionsFn,
+  UserFlowContext,
+  UserFlowProvider,
+  LhConfigJson,
+  ReportFormat
+} from '@push-based/user-flow';
+
 export type UserFlowProject = Project & {
   $init: ExecFn<Partial<InitCommandArgv & GlobalOptionsArgv>>,
   $collect: ExecFn<Partial<CollectCommandArgv & GlobalOptionsArgv>>,
