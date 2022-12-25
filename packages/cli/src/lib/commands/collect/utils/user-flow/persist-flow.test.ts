0fd48386e97ba3244715ec57a4e6a7cdfd94b840
Michael Hladky <michael@hladky.at>
2022-12-25 19:14:21 +0100

    test: use test data from test-data lib

--- packages/cli/src/lib/commands/collect/utils/user-flow/persist-flow.test.ts
+++ packages/cli/src/lib/commands/collect/utils/user-flow/persist-flow.test.ts
@@ -4,8 +4,7 @@ import FlowResult from 'lighthouse/types/lhr/flow';
 import { persistFlow } from './persist-flow';
 import { ReportFormat } from '../../options/types';
 import { UserFlowCliProject, UserFlowCliProjectFactory } from '@push-based/user-flow-cli-testing';
-import { INITIATED_PRJ_CFG } from '../../../../../../tests/fixtures/sandbox/initiated';
-import { getReportContent } from '../../../../../../test-data/raw-reports';
+import { getReportContent, INITIATED_PRJ_CFG } from 'test-data';
 import { PersistFlowOptions } from './types';
 import { dateToIsoLikeString, toReportName } from './utils';
 
