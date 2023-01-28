ff739e16bd4caf71a45227a6f9f1ca927d697be1
Michael Hladky <michael@hladky.at>
2023-01-28 09:03:56 +0100

    refactor: test headless:true

--- packages/cli/src/lib/commands/collect/utils/user-flow/collect-flow.ts
+++ packages/cli/src/lib/commands/collect/utils/user-flow/collect-flow.ts
@@ -44,7 +44,7 @@ export async function collectFlow(
   // @TODO consider CI vs dev mode => headless, openReport, persist etc
   const cliMode = detectCliMode();
   if (cliMode !== 'DEFAULT') {
-    const headlessMode = false;
+    const headlessMode = true;
     logVerbose(`Set headless to ${headlessMode} as we are running in ${cliMode} mode`);
     launchOptions.headless = headlessMode;
   }
