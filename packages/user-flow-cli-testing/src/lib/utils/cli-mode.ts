6e3ce66693f83d11bb44746526328cc82070398c
Michael Hladky <michael@hladky.at>
2023-01-28 18:15:07 +0100

    refactor: adjust env helpers 2

--- packages/user-flow-cli-testing/src/lib/utils/cli-mode.ts
+++ packages/user-flow-cli-testing/src/lib/utils/cli-mode.ts
@@ -13,6 +13,7 @@ export function setupEnvVars(env: CLI_MODES): void {
 export function teardownEnvVars() {
   delete process.env[CI_PROPERTY];
   delete process.env[CLI_MODE_PROPERTY];
+  delete process.env['GITHUB_ACTIONS'];
 }
 
 // @TODO: move into cli-project as it deals with env vars
