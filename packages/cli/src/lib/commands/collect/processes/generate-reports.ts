5ab06c5141ec0aff6cbee49c64a4a2c68a18b212
Chris <chris@deep-blue.io>
2023-01-30 13:54:37 +0100

    Revert "refactor: adjust env helpers 3"
    This reverts commit ea5a929d0f96052e3884b01d27ed7789a9106f93.

--- packages/cli/src/lib/commands/collect/processes/generate-reports.ts
+++ packages/cli/src/lib/commands/collect/processes/generate-reports.ts
@@ -7,19 +7,16 @@ import { userFlowReportToMdTable } from "../../assert/utils/md-table";
 
 export function createReducedReport(flowResult: FlowResult): ReducedReport {
   const steps = flowResult.steps.map((step) => {
-    let results: ReducedFlowStepResult
-    if(step.lhr) {
-      const stepReport = Util.prepareReportResult(step.lhr);
-      const { gatherMode } = stepReport;
-      const categoriesEntries: [string, LHR.Category][] = Object.entries(stepReport.categories) as unknown as [string, LHR.Category][];
-      results = categoriesEntries.reduce((res, [categoryName, category]) => {
-        res[categoryName] = Util.shouldDisplayAsFraction(stepReport.gatherMode) ?
-          Util.calculateCategoryFraction(category): (category).score;
-        return res
-      }, {} as ReducedFlowStepResult);
-      return { name: step.name, gatherMode, results };
-    } else return undefined as any
-  }).filter(v => v !== undefined);
+    const stepReport = Util.prepareReportResult(step.lhr);
+    const { gatherMode } = stepReport;
+    const categoriesEntries: [string, LHR.Category][] = Object.entries(stepReport.categories) as unknown as [string, LHR.Category][];
+    const results: ReducedFlowStepResult = categoriesEntries.reduce((res, [categoryName, category]) => {
+      res[categoryName] = Util.shouldDisplayAsFraction(stepReport.gatherMode) ?
+        Util.calculateCategoryFraction(category): (category).score;
+      return res
+    }, {} as ReducedFlowStepResult);
+    return { name: step.name, gatherMode, results };
+  });
   return {name: flowResult.name, steps};
 }
 
