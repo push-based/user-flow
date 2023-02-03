0e9d4b741bb0bb9fbc54d3aedcaa5ef78e21f74f
Michael Hladky <michael@hladky.at>
2023-02-03 10:00:38 +0100

    refactor: update user flow to more complex case

--- packages/user-flow-ci-integration/user-flows/order-coffee.uf.ts
+++ packages/user-flow-ci-integration/user-flows/order-coffee.uf.ts
@@ -3,21 +3,45 @@ import {
   UserFlowContext,
   UserFlowProvider
 } from '@push-based/user-flow';
+import { Coffee } from '../ufo/coffee.ufo';
+import { CheckoutForm } from '../ufo/checkout.form';
+import { formData } from '../data/checkout.data';
 
 // Your custom interactions with the page
 const interactions: UserFlowInteractionsFn = async (ctx: UserFlowContext): Promise<any> => {
-  const { page, flow, browser, collectOptions } = ctx;
+  const { flow, collectOptions } = ctx;
   const { url } = collectOptions;
+
+  const coffeeUfo = new Coffee(ctx);
+  const checkoutFormUfo = new CheckoutForm(ctx);
 
   // Navigate to coffee order site
   await flow.navigate(url, {
-    stepName: 'Navigate to coffee cart',
+    stepName: 'Navigate to coffee cart'
   });
 
+  await flow.startTimespan({ stepName: 'Select coffee' });
+  // Select coffee
+  coffeeUfo.selectCappuccino();
+  await flow.endTimespan();
+  await flow.snapshot({ stepName: 'Coffee selected' });
+
+  await flow.startTimespan({ stepName: 'Checkout order' });
+  // Checkout order
+  await checkoutFormUfo.openOrder();
+  await checkoutFormUfo.fillCheckoutForm(formData);
+  await flow.endTimespan();
+  await flow.snapshot({ stepName: 'Order checked out' });
+
+  await flow.startTimespan({ stepName: 'Submit order' });
+  // Submit order
+  await checkoutFormUfo.submitOrder();
+  await flow.endTimespan();
+  await flow.snapshot({ stepName: 'Order submitted' });
 };
 
 const userFlowProvider: UserFlowProvider = {
-  flowOptions: {name: 'Order Coffee in CI'},
+  flowOptions: { name: 'Order Coffee' },
   interactions
 };
 
