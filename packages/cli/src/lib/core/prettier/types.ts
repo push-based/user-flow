5d061f20fb85e3d9393bf39edeec969a9a01a7e2
Michael Hladky <michael@hladky.at>
2023-02-04 19:28:24 +0100

    feat: add static file and formatting for yml

--- packages/cli/src/lib/core/prettier/types.ts
+++ packages/cli/src/lib/core/prettier/types.ts
@@ -1,6 +1,6 @@
 import { BuiltInParserName } from 'prettier';
 
-export type SupportedParser = Extract<BuiltInParserName, 'css' | 'html' | 'json' | 'less' | 'scss' | 'markdown' | 'typescript'>;
+export type SupportedParser = Extract<BuiltInParserName, 'yaml' | 'css' | 'html' | 'json' | 'less' | 'scss' | 'markdown' | 'typescript'>;
 
 type SupportedParserNotExtname = Extract<SupportedParser, 'markdown' | 'typescript'>;
-export type SupportedExtname = Exclude<SupportedParser, SupportedParserNotExtname> | 'md' | 'ts' | 'js';
+export type SupportedExtname = Exclude<SupportedParser, SupportedParserNotExtname> | 'md' | 'ts' | 'js' | 'yml';
