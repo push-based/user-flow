d81a69f9726d86a8062d32b04d22ea107bbee88f
Michael Hladky <michael@hladky.at>
2023-02-09 15:43:15 +0100

    fix: rename script

--- tools/md-report-rename.ts
+++ tools/md-report-rename.ts
@@ -1,12 +1,14 @@
-import { readdirSync, writeFileSync } from 'fs';
+import { readdirSync, writeFileSync, readFileSync } from 'fs';
 import { join } from 'path';
+import { readFile } from '../dist/packages/cli/src/lib/core/file';
 
 console.log(`Reame results for comment action`);
 const path = 'packages/user-flow-ci-integration/measures';
-const reportJsonPath = readdirSync(path)[0];
-if (!reportJsonPath) {
+const reportPath = readdirSync(path)[0];
+if (!reportPath) {
   throw new Error('Report file not found');
 }
-const dest = join(path, reportJsonPath);
-writeFileSync('md-report.md', dest, { encoding: 'utf8' });
-console.log(`Report ${join(path, reportJsonPath)} renamed to 'md-report.md'`);
+const targetPath = join(path, reportPath);
+const destPath = join(path, 'md-report.md');
+writeFileSync(destPath, readFileSync(targetPath, {encoding: 'utf8'}));
+console.log(`Report ${targetPath} renamed to ${destPath}`);
