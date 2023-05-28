6be20b9f672bc7f159b62d919db57b4a10080637
Michael <michael.hladky@push-based.io>
2023-05-28 03:03:32 +0200

    feat: user-flow-plugin executor

---
+++ packages/plugin-user-flow/src/executors/test/executor.ts
@@ -0,0 +1,9 @@
+import { TestExecutorSchema } from './schema';
+
+export default async function runExecutor(options: TestExecutorSchema) {
+  console.log('Executor ran for Test', options);
+
+  return {
+    success: true,
+  };
+}
