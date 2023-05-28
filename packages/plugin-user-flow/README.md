4f025b8b016dc4541361b7663521f3dd97e97a69
Michael <michael.hladky@push-based.io>
2023-05-28 02:47:27 +0200

    feat: add nx plugin

---
+++ packages/plugin-user-flow/README.md
@@ -0,0 +1,11 @@
+# plugin-user-flow
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build plugin-user-flow` to build the library.
+
+## Running unit tests
+
+Run `nx test plugin-user-flow` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/plugin-user-flow/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'plugin-user-flow',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/plugin-user-flow',
+};
