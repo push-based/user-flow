fc911f04e04b13e9a68975b7a861920a3d624384
ChristopherPHolder <toholderandrews@gmail.com>
2023-12-19 14:05:15 +0100

    fix(esm): fix imports

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -7,8 +7,6 @@ import { runCli } from './lib/core/yargs/index.js';
 import { commands } from './lib/commands/commands.js';
 import { GLOBAL_OPTIONS_YARGS_CFG } from './lib/global/options/index.js';
 
-console.log('Runs')
-
 runCli({
   commands: commands,
   options: {...GLOBAL_OPTIONS_YARGS_CFG},
