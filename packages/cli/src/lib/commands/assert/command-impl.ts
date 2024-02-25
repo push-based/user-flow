8145193a674ae676a3248fcc3c741cd5ef6da103
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-25 16:37:40 +0100

    fix: remove unnecessary patch type errors

--- packages/cli/src/lib/commands/assert/command-impl.ts
+++ packages/cli/src/lib/commands/assert/command-impl.ts
@@ -1,7 +1,7 @@
 import { logVerbose } from '../../core/loggin';
 import { readBudgets } from './utils/budgets';
 
-export async function runAssertCommand() {
+export async function runAssertCommand(): Promise<void> {
   logVerbose(readBudgets());
   return Promise.resolve();
 }
--- packages/cli/src/lib/commands/assert/utils/budgets/index.ts
+++ packages/cli/src/lib/commands/assert/utils/budgets/index.ts
@@ -1,10 +1,10 @@
 import Budget from 'lighthouse/types/lhr/budget';
 import { readFile } from '../../../../core/file';
-import { DEFAULT_ASSERT_BUDGET_PATH } from '../../constants';
+
+const DEFAULT_ASSERT_BUDGET_PATH = './budget.json';
 
 export function readBudgets(budgetPath: string = DEFAULT_ASSERT_BUDGET_PATH): Budget[] {
-  const budgetsJson = JSON.parse(readFile(budgetPath, {fail: true}) || '{}');
-  return budgetsJson;
+  return JSON.parse(readFile(budgetPath, { fail: true }) || '{}');
 }
 
 
