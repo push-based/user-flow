1b3c6415fbd275b94a66c3d210cb6f2694c6f199
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-25 13:56:35 +0100

    fix: do not export internals

--- packages/cli/src/lib/commands/assert/options.ts
+++ packages/cli/src/lib/commands/assert/options.ts
@@ -1,13 +1,13 @@
 import { Options } from 'yargs';
 
-export const budgets = {
+const budgets = {
   alias: 'j',
   type: 'array',
   string: true,
   description: 'Performance budgets (RC file only)'
 } satisfies Options;
 
-export const budgetPath = {
+const budgetPath = {
   alias: 'k',
   type: 'string',
   description: 'Path to budgets.json'
