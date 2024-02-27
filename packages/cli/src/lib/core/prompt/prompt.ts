73ac637df746cdaa6d4ab9bab123d2459e996df1
Christopher Holder <40126819+ChristopherPHolder@users.noreply.github.com>
2024-02-28 00:38:01 +0100

    fix: remove mutation
    Co-authored-by: Katka Pilátová <tlacencin@gmail.com>

--- packages/cli/src/lib/core/prompt/prompt.ts
+++ packages/cli/src/lib/core/prompt/prompt.ts
@@ -1,12 +1,11 @@
 import { prompt } from 'enquirer';
 
 export async function promptParam<T>(cfg: {initial?: T, skip?: boolean, message: string, type?: any, [key: string]: any}): Promise<T> {
-  let { type, initial, message, skip, result } = cfg;
-  type = type || 'input';
+  const { type, initial, message, skip, result } = cfg;
 
   const { param } = await prompt<{ param: T }>([{
     name: 'param',
-    type,
+    type: type || 'input',
     initial,
     message,
     skip,
