88aab18af5de3db39b28ae882e4f8a2fef45f5f0
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-28 00:54:41 +0100

    fix: remove redundant checks in test

--- packages/cli/src/lib/core/prompt/confirm-to-process.unit.test.ts
+++ packages/cli/src/lib/core/prompt/confirm-to-process.unit.test.ts
@@ -9,63 +9,53 @@ describe('confirmToProcess', () => {
   const mockPrompt = 'Confirm should process?';
   const promptParamSpy = jest.spyOn(prompt, 'promptParam');
   const mockProcess = jest.fn();
-  const mockPrecondition = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
   })
 
-  it('should check if precondition is met', async () => {
-    mockPrecondition.mockResolvedValue(false);
+  it('should check if precondition is met if one is given', async () => {
+    const mockPrecondition = jest.fn();
     await confirmToProcess({
       prompt: mockPrompt,
       process: mockProcess,
       precondition: mockPrecondition,
-    })(CONTEXT);
+    })(EMPTY_CONTEXT);
     expect(mockPrecondition).toHaveBeenCalled();
   });
 
   it('should not prompt if precondition is not met', async () => {
-    mockPrecondition.mockResolvedValue(false);
     await confirmToProcess({
       prompt: mockPrompt,
       process: mockProcess,
-      precondition: mockPrecondition,
-    })(CONTEXT);
-    expect(mockPrecondition).toHaveBeenCalled();
+      precondition: () => false,
+    })(EMPTY_CONTEXT);
     expect(promptParamSpy).not.toHaveBeenCalled();
   });
 
   it('should prompt if precondition is met', async () => {
-    mockPrecondition.mockResolvedValue(true);
     await confirmToProcess({
       prompt: mockPrompt,
       process: mockProcess,
-      precondition: mockPrecondition,
-    })(CONTEXT);
-    expect(mockPrecondition).toHaveBeenCalled();
+    })(EMPTY_CONTEXT);
     expect(promptParamSpy).toHaveBeenCalled();
   });
 
   it('should not process if prompt is denied', async () => {
-    mockPrecondition.mockResolvedValue(true);
     promptParamSpy.mockResolvedValue(false);
     await confirmToProcess({
       prompt: mockPrompt,
       process: mockProcess,
-      precondition: mockPrecondition,
-    })(CONTEXT);
+    })(EMPTY_CONTEXT);
     expect(mockProcess).not.toHaveBeenCalled();
   });
 
   it('should process if prompt is accepted', async () => {
-    mockPrecondition.mockResolvedValue(true);
     promptParamSpy.mockResolvedValue(false);
     await confirmToProcess({
       prompt: mockPrompt,
       process: mockProcess,
-      precondition: mockPrecondition,
-    })(CONTEXT);
+    })(EMPTY_CONTEXT);
     expect(mockProcess).not.toHaveBeenCalled();
   });
 })
