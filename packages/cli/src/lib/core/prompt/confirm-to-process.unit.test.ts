a7c28dbd9c414fd1ec6bc5e9d7bf20dd867e5da1
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-28 01:18:47 +0100

    fix: reduce global test setup and move to cases

--- packages/cli/src/lib/core/prompt/confirm-to-process.unit.test.ts
+++ packages/cli/src/lib/core/prompt/confirm-to-process.unit.test.ts
@@ -6,9 +6,7 @@ jest.mock('./prompt');
 
 describe('confirmToProcess', () => {
   const EMPTY_CONTEXT = {} as RcJson;
-  const mockPrompt = 'Confirm should process?';
-  const promptParamSpy = jest.spyOn(prompt, 'promptParam');
-  const mockProcess = jest.fn();
+  const MOCK_PROMPT = 'Confirm should process?';
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -17,54 +15,59 @@ describe('confirmToProcess', () => {
   it('should check if precondition is met if one is given', async () => {
     const mockPrecondition = jest.fn();
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: jest.fn(),
       precondition: mockPrecondition,
     })(EMPTY_CONTEXT);
     expect(mockPrecondition).toHaveBeenCalled();
   });
 
   it('should not prompt if precondition is not met', async () => {
+    const promptParamSpy = jest.spyOn(prompt, 'promptParam');
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: jest.fn(),
       precondition: () => false,
     })(EMPTY_CONTEXT);
     expect(promptParamSpy).not.toHaveBeenCalled();
   });
 
   it('should prompt if precondition is met', async () => {
+    const promptParamSpy = jest.spyOn(prompt, 'promptParam');
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: jest.fn(),
       precondition: () => true,
     })(EMPTY_CONTEXT);
     expect(promptParamSpy).toHaveBeenCalled();
   });
 
   it('should prompt if no precondition is passed', async () => {
+    const promptParamSpy = jest.spyOn(prompt, 'promptParam');
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: jest.fn(),
     })(EMPTY_CONTEXT);
     expect(promptParamSpy).toHaveBeenCalled();
   });
 
   it('should not process if prompt is denied', async () => {
-    promptParamSpy.mockResolvedValue(false);
+    jest.spyOn(prompt, 'promptParam').mockResolvedValue(false);
+    const processSpy = jest.fn();
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: processSpy,
     })(EMPTY_CONTEXT);
-    expect(mockProcess).not.toHaveBeenCalled();
+    expect(processSpy).not.toHaveBeenCalled();
   });
 
   it('should process if prompt is accepted', async () => {
-    promptParamSpy.mockResolvedValue(true);
+    jest.spyOn(prompt, 'promptParam').mockResolvedValue(true);
+    const processSpy = jest.fn();
     await confirmToProcess({
-      prompt: mockPrompt,
-      process: mockProcess,
+      prompt: MOCK_PROMPT,
+      process: processSpy,
     })(EMPTY_CONTEXT);
-    expect(mockProcess).toHaveBeenCalled();
+    expect(processSpy).toHaveBeenCalled();
   });
 })
