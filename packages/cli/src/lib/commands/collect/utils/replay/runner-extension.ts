2cb7ca1b6af4a8b6d4790abd84c9d8d30d8ca8bc
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-22 10:34:18 +0200

    fix(cli): ignore replay runner errors

--- packages/cli/src/lib/commands/collect/utils/replay/runner-extension.ts
+++ packages/cli/src/lib/commands/collect/utils/replay/runner-extension.ts
@@ -13,16 +13,18 @@ export class UserFlowRunnerExtension extends PuppeteerRunnerExtension {
         super(browser, page, opts);
     }
 
-    // eslint-disable-next-line
-    // @ts-ignore
-    async runStep(step: UserFlowRecordingStep, flowRecording: UserFlowRecording): Promise<void> {
+  override async runStep(step: UserFlowRecordingStep, flowRecording: UserFlowRecording): Promise<void> {
         if (isMeasureType(step.type) && !this.flow?.currentTimespan) {
             const userFlowStep = step as MeasurementStep;
             const stepOptions = userFlowStep?.stepOptions;
             if (userFlowStep.type === 'navigate') {
-                return this.flow[userFlowStep.type](userFlowStep?.url, {...stepOptions});
+              // TODO fix (Changes to Flow API)
+                // @ts-ignore
+              return this.flow[userFlowStep.type](userFlowStep?.url, {...stepOptions});
             }
-            return this.flow[userFlowStep.type]({...stepOptions});
+            // TODO fix (Changes to Flow API)
+            // @ts-ignore
+          return this.flow[userFlowStep.type]({...stepOptions});
         } else {
             return super.runStep(step as Step, flowRecording);
         }
