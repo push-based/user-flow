ab14807e83a172e1c2f078204de2eb203f15b399
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-27 08:28:45 +0200

    test(e2e): setup e2e exec process base

--- e2e/cli-e2e/tests/assert.e2e.test.ts
+++ e2e/cli-e2e/tests/assert.e2e.test.ts
@@ -1,7 +1,15 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { executeProcess } from '@code-pushup/utils';
 
 describe('user-flow assert', () => {
-  it('should run test', () => {
+  it('should run test', async () => {
+    const { code, stdout, stderr } = await executeProcess({
+      command: 'echo',
+      args: ['value']
+    })
+    expect(code).toBe(0);
+    expect(stdout).toContain('value');
+    expect(stderr).toBe('');
     expect('value').toBeTruthy();
   })
 })
