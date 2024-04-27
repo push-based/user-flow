a88bc1d2ec3e2af99f101ea46ac99062de132ea2
ChristopherPHolder <toholderandrews@gmail.com>
2024-04-27 08:08:38 +0200

    test(e2e): setup e2e base

---
+++ e2e/cli-e2e/vite.config.e2e.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vite/cli-e2e',
+
+  plugins: [nxViteTsPaths()],
+
+  test: {
+    globals: true,
+    reporters: ['basic'],
+    testTimeout: 120_000,
+    // alias: tsconfigPathAliases(),
+    pool: 'threads',
+    poolOptions: { threads: { singleThread: true } },
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'node',
+    include: ['tests/**/*.e2e.test.ts'],
+    // globalSetup: ['../../global-setup.e2e.ts'],
+    // setupFiles: ['../../testing/test-setup/src/lib/reset.mocks.ts'],
+  },
+});
