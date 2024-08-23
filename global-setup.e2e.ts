0b063b0d474ae7789299c33d73f393eb5125ed92
ChristopherPHolder <toholderandrews@gmail.com>
2024-08-23 21:11:51 +0200

    fix(workspace): migrate nx

--- global-setup.e2e.ts
+++ global-setup.e2e.ts
@@ -3,11 +3,13 @@ import startLocalRegistry from './tools/scripts/start-local-registry';
 import stopLocalRegistry from './tools/scripts/stop-local-registry';
 
 export async function setup() {
+  console.log('Starting local registry...');
   await startLocalRegistry();
   execSync('npm install -D @push-based/user-flow@1.0.0');
 }
 
 export async function teardown() {
+  console.log('Teardown registry')
   stopLocalRegistry();
   execSync('npm uninstall @push-based/user-flow');
 }
