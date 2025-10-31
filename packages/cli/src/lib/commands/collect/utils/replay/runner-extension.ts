dca4928254576d80e8a312927b317ff355534928
ChristopherPHolder <toholderandrews@gmail.com>
2025-10-31 12:44:18 +0100

    fix: fix replay navigation event

--- packages/cli/src/lib/commands/collect/utils/replay/runner-extension.ts
+++ packages/cli/src/lib/commands/collect/utils/replay/runner-extension.ts
@@ -1,32 +1,34 @@
-import {PuppeteerRunnerExtension, Step, UserFlow as UserFlowRecording} from "@puppeteer/replay";
-import {Browser, Page} from "puppeteer";
+import {
+  PuppeteerRunnerExtension,
+  Step,
+  UserFlow as UserFlowRecording,
+} from '@puppeteer/replay';
+import { Browser, Page } from 'puppeteer';
 import { MeasurementStep, UserFlowRecordingStep } from './types.js';
-import {isMeasureType} from "./utils.js";
-// @ts-ignore
-import {UserFlow} from 'lighthouse';
+import { isMeasureType } from './utils.js';
+import { UserFlow } from 'lighthouse';
 
 export class UserFlowRunnerExtension extends PuppeteerRunnerExtension {
+  constructor(
+    browser: Browser,
+    page: Page,
+    private flow: UserFlow,
+    opts?: {
+      timeout?: number;
+    },
+  ) {
+    super(browser, page, opts);
+  }
 
-    constructor(browser: Browser, page: Page, private flow: UserFlow, opts?: {
-        timeout?: number;
-    }) {
-        super(browser, page, opts);
+  override async runStep(
+    step: UserFlowRecordingStep,
+    flowRecording: UserFlowRecording,
+  ): Promise<void> {
+    if (!isMeasureType(step.type)) {
+      return super.runStep(step as Step, flowRecording);
     }
-
-  override async runStep(step: UserFlowRecordingStep, flowRecording: UserFlowRecording): Promise<void> {
-        if (isMeasureType(step.type) && !this.flow?.currentTimespan) {
-            const userFlowStep = step as MeasurementStep;
-            const stepOptions = userFlowStep?.stepOptions;
-            if (userFlowStep.type === 'navigate') {
-              // TODO fix (Changes to Flow API)
-                // @ts-ignore
-              return this.flow[userFlowStep.type](userFlowStep?.url, {...stepOptions});
-            }
-            // TODO fix (Changes to Flow API)
-            // @ts-ignore
-          return this.flow[userFlowStep.type]({...stepOptions});
-        } else {
-            return super.runStep(step as Step, flowRecording);
-        }
-    }
+    const userFlowStep = step as MeasurementStep;
+    const stepOptions = userFlowStep?.stepOptions;
+    return this.flow[userFlowStep.type]({ ...stepOptions });
+  }
 }
